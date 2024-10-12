097fa5a6188f546ed83a2bf2ac2ab1c538108837
bhuwan1301 <bhuwancp1301@gmail.com>
2024-10-12 15:22:50 +0530

    ONLY A LINK NEEDS TO BE ADDED TO PRODUCTS ON HOME PAGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layouts/layout";
-import { Home, About, Contact, Products, ProductPage, Error404 } from "./pages";
+import { Home, About, Contact, Products } from "./pages";
 import Provider from "./provider";
 function App() {
   return (
@@ -8,12 +8,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="*" element={<Error404 />} />
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="products" element={<Products />} />
-          <Route path="product/:slug" element={<ProductPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
